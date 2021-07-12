fdddaf5d6012c3b78da392cdcc3e2e5abae76f06
Mike H <72580577+mekanik20@users.noreply.github.com>
2021-07-11 21:00:48 -0400

    Added 'was deleted' status

--- index.js
+++ index.js
@@ -166,8 +166,9 @@ app.delete('/users/:Username', (req, res) => {
     .then((user) => {
       if (!user) {
         res.status(400).send(req.params.Username + 'was not found');
-      }
-    })
+      } else {
+        res.status(200).send(req.params.Username + " was deleted.");
+    }})
     .catch((err) => {
       console.error(err);
       res.status(500).send('Error: ' + err);
