38d6afa25c29a86aa18bdca03319bc821d469cd1
Mike H <72580577+mekanik20@users.noreply.github.com>
2021-08-18 15:41:46 -0400

    Fix for password error finding

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (r
 
 app.post('/users', [
   check('Username', 'Username is required').isLength({min: 5}),
-  check('Username', 'Username contains non alphanumeric characters - not allowed').isAlphanumeric(),
+  check('Password', 'Username contains non alphanumeric characters - not allowed').isAlphanumeric(),
   check('Password', 'Password is required').not().isEmpty(),
   check('Email', 'Email does not appear to valid').isEmail()
   ], (req, res) => {
