13ff58b01890404cf592a7ab1a09d787824d11d3
Mike H <72580577+mekanik20@users.noreply.github.com>
2021-10-08 14:51:13 -0400

    updated get movies with jwt token

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get('/', (req, res) => {
   res.send('Welcome to MyFlix.');
 });
 
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
